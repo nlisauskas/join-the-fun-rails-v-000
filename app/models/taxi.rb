c6fe7724801face709560a744c05011a48188121
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-23 04:05:31 +0000

    automatically backed up by learn

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,5 +1,5 @@
 class Taxi < ActiveRecord::Base
   has_many :rides
   has_many :taxi_passengers
-  has_many :passengers, through: :has_many :taxi_passengers
+  has_many :passengers, through: :taxi_passengers
 end
